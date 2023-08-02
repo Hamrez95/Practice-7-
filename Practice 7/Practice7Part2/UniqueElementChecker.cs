9e3fefe836f2a771cce2481fbe8a622d2e7033c8
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-08-02 14:53:00 +0330

    Practice7-Part2 Added to the program

---
+++ Practice 7/Practice7Part2/UniqueElementChecker.cs
@@ -0,0 +1,40 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+
+namespace Practice_7.Practice7Part2
+{
+    public class UniqueElementChecker
+    {
+        public int CountOfUniqueItems(int[] nums)
+        {
+            // Check if the input array is empty.
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+                
+
+            // use stack for create ordered list.
+            Stack<int> uniqueStack = new Stack<int>();
+
+            
+            foreach (int num in nums)
+            {
+                // Check if the element is not already in the Stack.
+                if (!uniqueStack.Contains(num))
+                {
+                    uniqueStack.Push(num);    // If its not in the Stack, add it to the Stack.
+                }
+            }
+
+            // items in the Stack.
+            return uniqueStack.Count;
+        }
+    }
+}
--- Practice 7/Program.cs
+++ Practice 7/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Practice_7.Practice7Part1;
+using Practice_7.Practice7Part2;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace BalancedParentheses
 {
@@ -10,6 +12,8 @@ namespace BalancedParentheses
         {
             #region FirstPractice   
             CharChecker checker = new CharChecker();
+
+            Console.WriteLine("Practice7-Part1");
 
             Console.WriteLine("Example 1:");
             Console.WriteLine("Input: '(', ')' ");
@@ -26,8 +30,25 @@ namespace BalancedParentheses
             Console.WriteLine("\nExample 4:");
             Console.WriteLine("Input: '(', '[', ']', ')', '{', '}', '[', ']', '{', '(', ')' ");
             Console.WriteLine("Output: " + checker.IsValid(new char[] { '(', '[', ']', ')', '{', '}', '[', ']', '{', '(', ')' }));
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------");
             #endregion
 
+            #region SecondPractice
+
+            Console.WriteLine("\nPractice7-Part2");
+            // Sample 
+            int[] nums1 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 66, 77, 77, 777 };
+
+            
+            UniqueElementChecker uniqueElementChecker = new UniqueElementChecker();
+
+            // Call the CountOfUniqueItems method 
+            int countOfUniqueItems = uniqueElementChecker.CountOfUniqueItems(nums1);
+
+            
+            Console.WriteLine("\nInput: nums1 = [ 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 66, 77, 77, 777 ]");
+            Console.WriteLine("Output: " + countOfUniqueItems);
+            #endregion
         }
 
     }
