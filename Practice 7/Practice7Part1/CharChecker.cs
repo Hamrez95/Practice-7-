a13a116782c0305e414559bcf78d8a82035eb133
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-08-02 14:03:31 +0330

    Add project files.

---
+++ Practice 7/Practice7Part1/CharChecker.cs
@@ -0,0 +1,41 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+#endregion
+
+
+namespace Practice_7.Practice7Part1
+{
+    public class CharChecker
+    {
+        public bool IsValid(char[] s)
+        {
+            Stack<char> stack = new Stack<char>(); //store char in the stack
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c); //add char to stack
+                }
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || !IsMatchingBracket(stack.Pop(), c))
+                        return false; // check empty stack and dismached char
+                }
+                
+            }
+
+            return stack.Count == 0; // Return true if all brackets are balanced
+        }
+
+        private bool IsMatchingBracket(char open, char close) //logic section of chars
+        {
+            return (open == '(' && close == ')') || (open == '{' && close == '}') || (open == '[' && close == ']');
+        }
+    }
+}
---
+++ Practice 7/Program.cs
@@ -0,0 +1,36 @@
+﻿using Practice_7.Practice7Part1;
+using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region FirstPractice   
+            CharChecker checker = new CharChecker();
+
+            Console.WriteLine("Example 1:");
+            Console.WriteLine("Input: '(', ')' ");
+            Console.WriteLine("Output: " + checker.IsValid(new char[] { '(', ')' }));
+
+            Console.WriteLine("\nExample 2:"); // \n for new line
+            Console.WriteLine("Input: '(', '[', ']', '{', '}', ')' ");
+            Console.WriteLine("Output: " + checker.IsValid(new char[] { '(', '[', ']', '{', '}', ')' }));
+
+            Console.WriteLine("\nExample 3:");
+            Console.WriteLine("Input: '(', ']' ");
+            Console.WriteLine("Output: " + checker.IsValid(new char[] { '(', ']' }));
+
+            Console.WriteLine("\nExample 4:");
+            Console.WriteLine("Input: '(', '[', ']', ')', '{', '}', '[', ']', '{', '(', ')' ");
+            Console.WriteLine("Output: " + checker.IsValid(new char[] { '(', '[', ']', ')', '{', '}', '[', ']', '{', '(', ')' }));
+            #endregion
+
+        }
+
+    }
+}
+
+
